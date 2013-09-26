fce1b276dbf27cbfd346d016652a47feb72dfb56
JP Richardson <jprichardson@gmail.com>
2013-09-26 12:55:22 -0500

    added test whens

--- test/jsock.test.js
+++ test/jsock.test.js
@@ -6,27 +6,33 @@ var testutil = require('testutil')
 var PORT = 45643
 
 describe('+ jsock', function() {
-  it('should communicate with another tcp socket', function(done) {
-    var server, flow;
+  describe('> when two node.js connections are made', function() {
+    it('should communicate with another tcp socket', function(done) {
+      var server, flow;
 
-    next(flow={
-      ERROR: done,
-      createServer: function() {
-        var server = net.createServer(function(client) {
-          client = jsock(client)
-          client.on('data', function(data) {
-            EQ (data.type, 'message')
-            EQ (data.contents, 'hello man!')
-            done()
+      next(flow={
+        ERROR: done,
+        createServer: function() {
+          var server = net.createServer(function(client) {
+            client = jsock(client)
+            client.on('data', function(data) {
+              EQ (data.type, 'message')
+              EQ (data.contents, 'hello man!')
+              done()
+            })
           })
-        })
-        server.listen(PORT, flow.next)
-      },
-      createClient: function() {
-        var client = jsock(net.createConnection(PORT))
-        client.write({type: 'message', contents: 'hello man!'})
-      }
+          server.listen(PORT, flow.next)
+        },
+        createClient: function() {
+          var client = jsock(net.createConnection(PORT))
+          client.write({type: 'message', contents: 'hello man!'})
+        }
+      })
     })
+  })
+
+  describe('> when there is an error', function() {
+    // body...
   })
 })
 
