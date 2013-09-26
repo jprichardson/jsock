3396b566b15f624b5697f4d7b6a04bfe04fc6cc8
JP Richardson <jprichardson@gmail.com>
2013-09-26 09:10:11 -0500

    implemented basic test

--- test/jsock.test.js
+++ test/jsock.test.js
@@ -1,8 +1,33 @@
 var testutil = require('testutil')
   , jsock = require('../lib/jsock')
+  , net = require('net')
+  , next = require('nextflow')
+
+var PORT = 45643
 
 describe('+ jsock', function() {
+  it('should communicate with another tcp socket', function(done) {
+    var server, flow;
 
+    next(flow={
+      ERROR: done,
+      createServer: function() {
+        var server = net.createServer(function(client) {
+          client = jsock(client)
+          client.on('data', function(data) {
+            EQ (data.type, 'message')
+            EQ (data.contents, 'hello man!')
+            done()
+          })
+        })
+        server.listen(PORT, flow.next)
+      },
+      createClient: function() {
+        var client = jsock(net.createConnection(PORT))
+        client.write({type: 'message', contents: 'hello man!'})
+      }
+    })
+  })
 })
 
 
